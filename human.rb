5e72b5a329906b15f365bc65e3cf134ee94897b7
sugiyamaF <fuka0712fuka@gmail.com>
2023-02-21 11:42:17 +0000

    finish2

--- human.rb
+++ human.rb
@@ -5,7 +5,7 @@ class Human < Animal
   include Thinkable
   attr_accessor :hobby
   
-  def initialize(hobby)
+  def initialize(name, old, hobby)
     self.name = name
     self.old = old
     self.hobby = hobby
--- main.rb
+++ main.rb
@@ -1,19 +1,13 @@
-require "./animal"
 require "./human"
 
-animal = Animal.new("田中 太郎", 25)
-animal.say
-tanaka = Human.new("電車")
+tanaka = Human.new("田中 太郎", 25, "電車")
+tanaka.say
 tanaka.think
 
-animal = Animal.new("鈴木 次郎", 30)
-animal.say
-suzuki = Human.new("野球")
+suzuki = Human.new("鈴木 次郎", 30, "野球")
+suzuki.say
 suzuki.think
 
-animal = Animal.new("佐藤 花子", 20)
-animal.say
-sato   = Human.new("映画")
+sato = Human.new("佐藤 花子", 20, "映画")
+sato.say
 sato.think
-
-
